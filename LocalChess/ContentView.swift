cbeaa0136f59edbc26a2385dc006798a4fe280e5
KrohnicDev <krohn.joonas@gmail.com>
2020-05-25 20:13:48 +0300

    Initial Commit

---
+++ LocalChess/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LocalChess
+//
+//  Created by Joonas Junttila on 25.5.2020.
+//  Copyright © 2020 Joonas Krohn. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
